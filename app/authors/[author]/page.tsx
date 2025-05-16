e9c4adf8b566d872bdc84fac25dc1c9b770c7e1a
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2025-05-16 20:10:36 +0000

    Make the authors page dynamic

--- app/authors/[author]/page.tsx
+++ app/authors/[author]/page.tsx
@@ -6,14 +6,16 @@ import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'Author' })
 
-export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'Stephanie_Viveros') as Authors
-  const mainContent = coreContent(author)
+export default async function Page(props: { params: Promise<{ author: string }> }) {
+  const params = await props.params
+  const { author } = params
+  const authorContent = allAuthors.find((p) => p.slug === author) as Authors
+  const mainContent = coreContent(authorContent)
 
   return (
     <>
       <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
+        <MDXLayoutRenderer code={authorContent.body.code} />
       </AuthorLayout>
     </>
   )
