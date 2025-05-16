ea6fbd9c2c7afe3a2f44c29b8527f7fdd217f5af
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2025-05-16 01:45:07 +0000

    Modify next config

--- next.config.js
+++ next.config.js
@@ -9,13 +9,13 @@ const withBundleAnalyzer = withBundleAnalyzerFactory({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-inline' giscus.app analytics.umami.is https://cusdis.com;
+  style-src 'self' 'unsafe-inline' https://cusdis.com;
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src giscus.app cusdis.com;
 `
 
 const securityHeaders = [
